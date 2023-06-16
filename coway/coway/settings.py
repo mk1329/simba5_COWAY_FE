efa7a0a947ffb57c529cab2dc3b5bbd790f5e1dc
mk1329 <mk1329@dgu.ac.kr>
2023-06-16 14:25:37 +0900

    Feat: nav랑 footer 상속

--- coway/coway/settings.py
+++ coway/coway/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'coway.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'coway' 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'coway', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
