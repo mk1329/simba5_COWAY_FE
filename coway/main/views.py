194896e026ab513da5041abe43267b7d0bd6bb65
SeolHyeona01 <sha0209@dgu.ac.kr>
2023-06-16 15:13:58 +0900

    연결

--- coway/main/views.py
+++ coway/main/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def mainpage(request):
     return render(request, 'main/main.html')
+
+def convpage(request):
+    return render(request, 'main/conv/conv.html')
+
+def convinfopage(request):
+    return render(request, 'main/conv/conv_info.html')
+
+def convmappage(request):
+    return render(request, 'main/conv/conv_map.html')
