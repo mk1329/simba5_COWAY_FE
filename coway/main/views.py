f16f7ef4d2fcb789daa0418d83ad9e57662d7f56
SeolHyeona01 <sha0209@dgu.ac.kr>
2023-06-16 15:33:06 +0900

    추가

--- coway/main/views.py
+++ coway/main/views.py
@@ -12,3 +12,12 @@ def convinfopage(request):
 
 def convmappage(request):
     return render(request, 'main/conv/conv_map.html')
+
+def easyroadpage(request):
+    return render(request, 'main/road/easy_road.html')
+
+def roadchoicepage(request):
+    return render(request, 'main/road/road_choice.html')
+
+def shortroadpage(request):
+    return render(request, 'main/road/short_road.html')
